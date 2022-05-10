5e182641b85ebac54fb7aeb26c37b61fce26db80
Sam <salonso@grupopm.mx>
2022-05-10 14:18:20 -0500

    coneccion a la bd

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -1,16 +1,13 @@
-import { Request, Response } from "express";
-
+import{Request, Response} from 'express';
 import pool from '../database';
 
-
 class GamesController{
-
-   public index(req:Request, res: Response){
-        res.send('Games')
+    public index(req: Request, res: Response){
+        pool.query('DESCRIBE games')
+        res.json('games')
+        
     }
-
 }
-
-const gamesController = new  GamesController();
+const gamesController = new GamesController();
 export default gamesController;
  
